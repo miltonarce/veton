ae302d8f0d2e3d008b36a8a67c29d339eb43da5e
JonathanSantos83 <jonathanmatias_santos@yahoo.com.ar>
2019-09-21 20:51:55 -0300

    BackAltaHistoriaClinicaMascotaUsuarioVeterinario-B09---AgregoSeedsRosario

---
+++ back/database/seeds/ConsultationsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ConsultationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('consultations')->insert([
+            'id_consultation' => 1,
+            'date' => '2019-09-21 13:45:12',
+            'comments' => 'Test 1 comment on consultations table',
+            'hide_comments' => 'Test 1 hidden comments on consultations table',
+            'afflictions_procedures' => 'Test 1 addfiction procedures on consultations table',
+            'image' => '',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('consultations')->insert([
+            'id_consultation' => 2,
+            'date' => '2019-09-21 14:45:12',
+            'comments' => 'Test 2 comment on consultations table',
+            'hide_comments' => 'Test 2 hidden comments on consultations table',
+            'afflictions_procedures' => 'Test 2 addfiction procedures on consultations table',
+            'image' => '',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
