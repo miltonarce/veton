573d5bb2b18783ae725096bd38e17547ffa113c4
matias torre <mtorre4580@outlook.com>
2019-09-22 15:40:14 -0300

    feat: add react-router with header,footer,main

---
+++ front/src/views/Contact/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Contact extends React.PureComponent {
+
+    render() {
+        return (
+            <React.Fragment>
+                <p>próximamente</p>
+            </React.Fragment>
+        );
+    }
+}
---
+++ front/src/views/Home/index.jsx
@@ -0,0 +1,13 @@
+import './index.scss'
+import React from 'react';
+
+export default class Home extends React.PureComponent {
+
+    render() {
+        return (
+            <React.Fragment>
+                <p>home</p>
+            </React.Fragment>
+        );
+    }
+}
