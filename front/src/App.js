d3fcabdbc415710ac6f4da8d09c4919752ff522a
miltonarce <miltonhernan.arce@gmail.com>
2019-09-27 20:54:58 -0300

    Sass base agregado

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import './assets/sass/styles.scss';
+
 // Template Header, Main, Footer
 import Header from './Components/Shared/Header/index';
 import Footer from './Components/Shared/Footer/index';
