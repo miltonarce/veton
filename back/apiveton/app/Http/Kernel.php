7199aab346dd02e0f4e195e235764822db38ca0b
miltonarce <miltonhernan.arce@gmail.com>
2019-09-27 20:13:29 -0300

    Agregado de CORS para laravel.

--- back/apiveton/app/Http/Kernel.php
+++ back/apiveton/app/Http/Kernel.php
@@ -38,6 +38,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            // Asociamos el Middleware de CORS a todas las rutas de api.
+            \App\Http\Middleware\CORS::class,
             'throttle:60,1',
             'bindings',
         ],
