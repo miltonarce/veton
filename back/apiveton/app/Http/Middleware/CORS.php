344c6eed1c145ab48d120286b4ebd34310ed580f
miltonarce <miltonhernan.arce@gmail.com>
2019-09-27 20:13:54 -0300

    Agregado de cors para laravel

---
+++ back/apiveton/app/Http/Middleware/CORS.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CORS
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+       // Este middleware va a agregar los headers para CORS.
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
+            ->header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type')
+            ->header('Access-Control-Allow-Credentials', 'true');
+    }
+}
