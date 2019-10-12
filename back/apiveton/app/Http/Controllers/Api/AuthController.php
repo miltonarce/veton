f332a50d8a60a0ca09aa4b0f57c69f61d4a316f6
matias torre <mtorre4580@outlook.com>
2019-10-12 14:54:46 -0300

    feat: add auth endpoints (login, register, logout)

---
+++ back/apiveton/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Role;
+use Auth;
+use Hash;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request) 
+    {
+        $request->validate(User::$rules);
+        if (Auth::attempt(['password' => $request['password'], 'email' => $request['email']])) {
+            return response()->json(['success' => true, 'additional_info' => $this->getAditionalInfo(Auth::user())]);
+    	}
+        return response()->json(['success' => false, 'msg' => 'Datos incorrectos']);
+    }
+
+    public function logout(Request $request) 
+    {
+        Auth::logout();
+        return response()->json(['success' => false, 'msg' => 'Se deslogueo correctamente!']);
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            $request->validate(User::$rules);
+            $user = $request->all();
+            $user['password'] = Hash::make($request['password']);
+            $user['id_role'] = 3;
+            User::create($user);
+            return response()->json(['success' => false, 'msg' => 'Se creo su usuario!']);
+        } catch (QueryException $e) {
+            return response()->json(['success' => false, 'msg' => 'Se produjo un error al crear su usuario']);
+        }
+    }
+
+    private function getAditionalInfo($data) {
+        return [
+            'name' => $data['name'],
+            'last_name' => $data['last_name'],
+            'dni' => $data['dni'],
+            'email' => $data['email'],
+            'birthday' => date('d-m-Y', strtotime($data['birthday'])),
+            'image' => $data['image'],
+        ];
+    }
+
+}
