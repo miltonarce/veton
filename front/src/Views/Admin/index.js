04356c4771e3bd2ec0ad495bd5f15b2d70bebc64
mtorre <daniel.torre@mercadolibre.com>
2019-11-28 22:02:53 -0300

    feat: deploy heroku

--- front/src/Views/Admin/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Admin extends React.Component {
-  render() {
-    return <p>admin view</p>;
-  }
-}
-
-export default Admin;
--- front/src/Views/User/HomeUser/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class HomeUser extends React.Component {
-  render() {
-    return <div>Home del usuario...</div>;
-  }
-}
-
-export default HomeUser;
