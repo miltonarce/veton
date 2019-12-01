a664cbfcee026d427207196e4d7622794d43b20a
rochix824 <rosario.carnelli@gmail.com>
2019-12-01 14:11:49 -0300

    perfiles de vet/user

--- front/src/Views/User/Profile/index.js
+++ front/src/Views/User/Profile/index.js
@@ -1,5 +1,11 @@
 import React from "react";
-import {Container, CssBaseline, Grid, Paper} from "@material-ui/core";
+import {
+  Container,
+  CssBaseline,
+  Grid,
+  Typography,
+  Paper,
+} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 
 import TitlePages from "../../../Components/TitlePages";
@@ -10,7 +16,33 @@ const useStyles = makeStyles(() => ({
   },
   Paper: {
     borderRadius: "23px",
-    padding: "2rem",
+    padding: "1.5rem",
+    height: "auto",
+  },
+  ContentCardUserProfile: {
+    marginTop: ".5rem",
+  },
+  ContentImage: {
+    position: "relative",
+    height: "8.56rem",
+  },
+  ImageUser: {
+    top: "-0.7rem",
+    width: "9.75rem",
+    height: "10rem",
+    position: "absolute",
+    boxShadow: "0px 3px 11px 5px rgba(0, 0, 0, 0.16)",
+    borderRadius: "123px",
+    left: "2rem", 
+  },
+  UserName: {
+    color: "#4E4E4E",
+    fontSize: "1.87rem",
+    fontWeight: 600,
+    marginBottom: ".2 rem",
+  },
+  Content: {
+    margin: ".75rem",
   },
 }));
 
@@ -31,10 +63,99 @@ const Profile = () => {
           className={classes.Main}
           direction="row"
           justify="center"
-          spacing={2}
+          spacing={3}
         >
-          <Grid item md={6} xs={12}>
-            <Paper className={classes.Paper}>asdasdasd</Paper>
+          <Grid item md={12} xs={12}>
+            <Grid
+              container
+              alignItems="center"
+              direction="row"
+              justify="space-between"
+              spacing={2}
+            >
+              <Grid item className={classes.ContentCardUserProfile} xs={10}>
+                <Paper className={classes.Paper}>
+                  <Grid
+                    container
+                    alignItems="center"
+                    className={classes.Paper}
+                    direction="row"
+                    justify="space-between"
+                    spacing={4}
+                  >
+                    <Grid item className={classes.ContentImage} xs={2}>
+                      <img
+                        alt=""
+                        className={classes.ImageUser}
+                        src="http://api.veton/imgs/simondiaz.jpg"
+                      />
+                    </Grid>
+                    <Grid item className={classes.Paper} xs={9}>
+                      <Grid
+                        container
+                        alignItems="center"
+                        className={classes.Content}
+                        direction="row"
+                        justify="flex-start"
+                        spacing={4}
+                      >
+                        <Grid item xs={8}>
+                          <Grid item xs={12}>
+                            <Typography
+                              className={classes.UserName}
+                              component="h3"
+                              variant="h3"
+                            >
+                              Nombre y Apellido
+                            </Typography>
+                          </Grid>
+                          <Grid item xs={12}>
+                            <Grid
+                              container
+                              alignItems="center"
+                              direction="row"
+                              justify="flex-start"
+                              spacing={4}
+                            >
+                              <Grid item xs={4}>
+                                <Grid item xs={12}>
+                                  <Typography color="secondary" component="p">
+                                    DNI
+                                  </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                  38.536.333
+                                </Grid>
+                              </Grid>
+                              <Grid item xs={4}>
+                                <Grid item xs={12}>
+                                  <Typography color="secondary" component="p">
+                                    EMAIL
+                                  </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                  testemail@gmail.com
+                                </Grid>
+                              </Grid>
+                              <Grid item xs={4}>
+                                <Grid item xs={12}>
+                                  <Typography color="secondary" component="p">
+                                    CUMPLEAÑOS
+                                  </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                  24-08-1994
+                                </Grid>
+                              </Grid>
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Paper>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Container>
